c3665a4dca915487f770554dd6e17b8dc9dad745
Yuwono Bangun Nagoro <morpheusgun@outlook.com>
2019-09-05 18:38:28 +0700

    add some test on runner, need to add more and more test, current coverage : 42.3 %

--- runner_test.go
+++ runner_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestNewFilter(t *testing.T) {
-	sos := []string{"Avenged Death", "Avenged Theater", "Avenged Corpse", "Avenged Theater", "Napalm Sevenfold", "Napalm Theater", "Napalm Corpse", "Dream Corpse", "Dream Sevenfold", "Dream Death", "Cannibal Theater", "Cannibal Death", "Cannibal Sevenfold"}
+	sos := []string{"Avenged Death", "Avenged Corpse", "Avenged Theater", "Napalm Sevenfold", "Napalm Theater", "Napalm Corpse", "Dream Corpse", "Dream Sevenfold", "Dream Death", "Cannibal Theater", "Cannibal Death", "Cannibal Sevenfold"}
 	filterRules := []FilterRule{
 		FilterRule{
 			FilterFunc: ContainsIgnoreCase,
@@ -24,5 +24,65 @@ func TestNewFilter(t *testing.T) {
 	}
 
 	actual := NewFilter(sos, filterRules)
+	assert.Equal(t, expected, actual)
+}
+
+func TestFilterStartsWithIgnoreCase(t *testing.T) {
+	sos := []string{"Avenged Death", "Avenged Corpse", "Avenged Theater", "Napalm Sevenfold", "Napalm Theater", "Napalm Corpse", "Dream Corpse", "Dream Sevenfold", "Dream Death", "Cannibal Theater", "Cannibal Death", "Cannibal Sevenfold"}
+	filterRules := []FilterRule{
+		FilterRule{
+			FilterFunc: StartsWithIgnoreCase,
+			Param:      "n",
+		},
+	}
+
+	expected := []string{"Napalm Sevenfold", "Napalm Theater", "Napalm Corpse"}
+	filter := NewFilter(sos, filterRules)
+	actual, _ := filter.Run()
+	assert.Equal(t, expected, actual)
+}
+
+func TestFilterStartsWith(t *testing.T) {
+	sos := []string{"Avenged Death", "Avenged Corpse", "Avenged Theater", "Napalm Sevenfold", "Napalm Theater", "Napalm Corpse", "Dream Corpse", "Dream Sevenfold", "Dream Death", "Cannibal Theater", "Cannibal Death", "Cannibal Sevenfold"}
+	filterRules := []FilterRule{
+		FilterRule{
+			FilterFunc: StartsWith,
+			Param:      "Av",
+		},
+	}
+
+	expected := []string{"Avenged Death", "Avenged Corpse", "Avenged Theater"}
+	filter := NewFilter(sos, filterRules)
+	actual, _ := filter.Run()
+	assert.Equal(t, expected, actual)
+}
+
+func TestFilterContainsIgnoreCase(t *testing.T) {
+	sos := []string{"Avenged Death", "Avenged Corpse", "Avenged Theater", "Napalm Sevenfold", "Napalm Theater", "Napalm Corpse", "Dream Corpse", "Dream Sevenfold", "Dream Death", "Cannibal Theater", "Cannibal Death", "Cannibal Sevenfold"}
+	filterRules := []FilterRule{
+		FilterRule{
+			FilterFunc: ContainsIgnoreCase,
+			Param:      "th",
+		},
+	}
+
+	expected := []string{"Avenged Death", "Avenged Theater", "Napalm Theater", "Dream Death", "Cannibal Theater", "Cannibal Death"}
+	filter := NewFilter(sos, filterRules)
+	actual, _ := filter.Run()
+	assert.Equal(t, expected, actual)
+}
+
+func TestFilterContains(t *testing.T) {
+	sos := []string{"Avenged Death", "Avenged Corpse", "Avenged Theater", "Napalm Sevenfold", "Napalm Theater", "Napalm Corpse", "Dream Corpse", "Dream Sevenfold", "Dream Death", "Cannibal Theater", "Cannibal Death", "Cannibal Sevenfold"}
+	filterRules := []FilterRule{
+		FilterRule{
+			FilterFunc: Contains,
+			Param:      "Th",
+		},
+	}
+
+	expected := []string{"Avenged Theater", "Napalm Theater", "Cannibal Theater"}
+	filter := NewFilter(sos, filterRules)
+	actual, _ := filter.Run()
 	assert.Equal(t, expected, actual)
 }
